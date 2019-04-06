bf92d29078fe749b1575130ee61dc26abd1c6e9b
Mercymuhonja <muhonjamercy63@gmail.com>
2019-04-06 12:39:25 -0700

    pushing new files to repository

---
+++ example2/calculator.py
@@ -0,0 +1,35 @@
+def add(a,b):
+	c=a+b
+	print(c)
+
+
+def subtract(d,e):
+	f=d-e
+	print(f)
+
+
+def multiply(a,b):
+	c=a*b
+	print(c)
+
+def divide(d,e):
+	f=d/e
+	print(f)
+
+def dividewhole(d,e):
+	f=d//e
+	print(f)
+
+
+def modulus(a,b):
+	c=a%b
+	print(c)
+
+
+def exponent(d,e):
+	f=d**e
+	print(f)
+
+def percentage(a,b):
+	c=(b/a)*100
+	print(c)
